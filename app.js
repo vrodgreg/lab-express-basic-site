bde55f4ca31b2a0da59a8443758c20a89250258d
vrodgreg <75463166+vrodgreg@users.noreply.github.com>
2021-02-23 16:19:11 -0500

    Lab Solved by Bob belcher

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+// require('dotenv').config()
+
+const app = express();
+
+app.use(express.static('public'))
+
+app.get('/home', (request, response, next) => {
+    response.sendFile(__dirname + '/views/home.html');
+  });
+
+  app.get('/about', (request, response, next) => {
+    response.sendFile(__dirname + '/views/about.html');
+  });
+
+  app.get('/works', (request, response, next) => {
+    response.sendFile(__dirname + '/views/works.html');
+  });
+
+//   const PORT = process.env.PORT || 3000
+  app.listen(3000, () => {
+    console.log(`Here we are listening on PORT 3000`)
+  } )
+  
+
+
+
